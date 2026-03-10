a855e498c0425d1389a49ad38dfd4bc7ed7c0629
Pascal Charbonneau <pcharbon70@gmail.com>
2026-03-10 10:50:50 -0600

    Add spec_led_ex dependency

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule LiveUi.MixProject do
     [
       {:jason, "~> 1.4"},
       {:jido_signal, "~> 1.0"},
+      {:spec_led_ex, git: "https://github.com/specleddev/specled_ex.git"},
       {:phoenix_live_view, "~> 1.0"},
       {:ex_doc, "~> 0.38", only: :dev, runtime: false}
     ]
